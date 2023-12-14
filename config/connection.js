26d604500b89f2c542acc01aa21c04d5e368caa3
Trevor Christensen <trevorchristensen3405@gmail.com>
2023-12-13 18:51:39 -0700

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.JAWSDB_NAME, process.env.JAWSDB_USER, process.env.JAWSDB_PASSWORD, {
-  host: process.env.JAWSDB_HOST,
-  port: process.env.JAWSDB_PORT,
+const sequelize = new Sequelize(process.env.JAWSDB_CONNECTION_STRING, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: 'Amazon RDS',
   },
 });
-
-// const sequelize = new Sequelize(process.env.JAWSDB_CONNECTION_STRING);
-
-module.exports = sequelize;
-
 
 module.exports = sequelize;
