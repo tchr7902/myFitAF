9b94eb91ff13532d5f5979940b0e27219a6d48c5
Trevor Christensen <trevorchristensen3405@gmail.com>
2023-12-13 19:58:44 -0700

    fix

--- db/database.js
+++ db/database.js
@@ -1,44 +1,44 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // Database connection setup using environment variables
-const db = mysql.createConnection({
-    host: process.env.JAWSDB_HOST,
-    user: process.env.JAWSDB_USER,
-    password: process.env.JAWSDB_PASSWORD,
-    database: process.env.JAWSDB_NAME
-});
+const sequelize = new Sequelize(process.env.JAWSDB_CONNECTION_STRING);
 
 // Function to store exercise plan
 const storeExercisePlan = (userId, exercisePlan) => {
-    exercisePlan.forEach(plan => {
-        const query = 'INSERT INTO exercise_plans (user_id, exercise_details) VALUES (?, ?)';
-        db.query(query, [userId, JSON.stringify(plan)], (err, result) => {
-            if (err) throw err;
-            console.log('Exercise plan stored for user:', userId);
-        });
+  exercisePlan.forEach(plan => {
+    sequelize.query('INSERT INTO exercise_plans (user_id, exercise_details) VALUES (?, ?)', {
+      replacements: [userId, JSON.stringify(plan)],
+      type: Sequelize.QueryTypes.INSERT,
+    }).then(result => {
+      console.log('Exercise plan stored for user:', userId);
+    }).catch(err => {
+      console.error('Error storing exercise plan:', err);
     });
+  });
 };
 
 // Function to store meal plan
 const storeMealPlan = (userId, mealPlan) => {
-    Object.entries(mealPlan).forEach(([day, meals]) => {
-        const query = 'INSERT INTO meal_plans (user_id, day, meal_details) VALUES (?, ?, ?)';
-        db.query(query, [userId, day, JSON.stringify(meals)], (err, result) => {
-            if (err) throw err;
-            console.log('Meal plan stored for user:', userId);
-        });
+  Object.entries(mealPlan).forEach(([day, meals]) => {
+    sequelize.query('INSERT INTO meal_plans (user_id, day, meal_details) VALUES (?, ?, ?)', {
+      replacements: [userId, day, JSON.stringify(meals)],
+      type: Sequelize.QueryTypes.INSERT,
+    }).then(result => {
+      console.log('Meal plan stored for user:', userId);
+    }).catch(err => {
+      console.error('Error storing meal plan:', err);
     });
+  });
 };
 
-function connectDatabase() {
-    db.connect(err => {
-        if (err) {
-            console.error('Error connecting to the database:', err);
-            process.exit(1);
-        }
-        console.log('Connected to MySQL database');
-    });
-}
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connected to MySQL database');
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  });
 
-module.exports = { storeExercisePlan, storeMealPlan, connectDatabase };
+module.exports = { storeExercisePlan, storeMealPlan };
